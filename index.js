e7c3924a2b604478b329365166eb773c982de68d
CarlJamison <31994209+CarlJamison@users.noreply.github.com>
2022-03-22 15:01:36 -0500

    clean up

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
-var players = 1;
-var connList = [];
-
 app.get('/controller', (req, res) => {
   res.sendFile(__dirname + '/misery-controller.html');
 });
@@ -13,22 +10,16 @@ app.get('/client', (req, res) => {
   res.sendFile(__dirname + '/misery-client.html');
 });
 
-app.get('/canvasSocket.js', (req, res) => {
-  res.sendFile(__dirname + '/canvasSocket.js');
-});
-
 var controllers = io.of("/controller")
 var clients = io.of("/client")
 
 controllers.on('connection', (socket) => {
 
-  var playerNumber = players;
-  players++;
-
   socket.on('message', msg => {
     clients.emit('message', msg);
     console.log(msg);
   });
+
 });
 
 clients.on('connection', (socket) => {
