b4f7db37be6e807068d4dab929d80a2d9a26e6f3
CarlJamison <31994209+CarlJamison@users.noreply.github.com>
2022-10-03 21:08:28 -0500

    add tablesssssssssssssssssssssssssssssssssssssssssssssssss

--- agent/modules/Processes/return-types.js
+++ agent/modules/Processes/return-types.js
@@ -3,11 +3,20 @@ types = {
     4: message => {
         var label = "Processes",
         id = "tabs-" + tabCounter,
-        li = $( tabTemplate.replace( /#\{href\}/g, "#" + id ).replace( /#\{label\}/g, label ) ),
-        tabContentHtml = message.output;
+        li = $( tabTemplate.replace( /#\{href\}/g, "#" + id ).replace( /#\{label\}/g, label ) );
+
+        var content = "";
+        var list = JSON.parse(message.output);
+        if(list.length){
+            content = `<table><tr><th>${Object.keys(list[0]).join("</th><th>")}</th></tr>`;
+            list.forEach(p => content += `<tr><td>${Object.values(p).join("</td><td>")}</tr></td>`);
+            content += "</table>"
+        }else{
+            content = "Nothing to display";
+        }
 
         tabs.find( ".ui-tabs-nav" ).append( li );
-        tabs.prepend( "<div id='" + id + "'><p>" + tabContentHtml + "</p></div>" );
+        tabs.prepend( "<div id='" + id + "'><p>" + content + "</p></div>" );
         tabs.tabs( "refresh" );
 
         tabCounter++;
