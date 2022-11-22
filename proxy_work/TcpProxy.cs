d3d714f605a5e4c640009a2c919da0f80445b305
CarlJamison <31994209+CarlJamison@users.noreply.github.com>
2022-11-21 22:34:36 -0600

    proxy poc

--- proxy_work/TcpProxy.cs
+++ proxy_work/TcpProxy.cs
@@ -1,22 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using SocketIOClient;
-using System.Net;
-using System.Security.Principal;
-using System.Net.NetworkInformation;
 using System.Net.Sockets;
-using System.Diagnostics;
-using System.IO;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Threading;
-using Microsoft.Win32;
-using System.Management;
-// TODO: Remove uneccesary imports
-
 
 namespace TcpProxy
 {
@@ -24,7 +7,7 @@ namespace TcpProxy
     {
         public static void Main(string[] args)
         {
-            if (args.Length > 1)
+            if (args.Any())
             {
                 Go(args[0]);
             }
@@ -34,70 +17,187 @@ namespace TcpProxy
             }
             while (true) { };
         }
-        static async void Go(string home = "http://192.168.1.103:8888/")
+
+        static async void Go(string home = "http://localHost:8000/", int targetPort = 3000, string targetHost = "localHost")
         {
-            // create new socket.io client
             var client = new SocketIO(home);
-
-            // register all of the commands
-            // Client echo
-            client.On("echo", response =>
-            {
-                // Echo service
-                Console.WriteLine(response.ToString());
-                client.EmitAsync("echo", response.ToString());
-
-            });
-
-            /*
-            Console.WriteLine("Starting relay");
-            relay();
-            Console.WriteLine("Moving past relay");
-
-            async void relay(string host = "127.0.0.1", int port = 445)
-            {
-                using (TcpClient tcpClient = new TcpClient(host, port))
-                {
-                    NetworkStream networkStream = tcpClient.GetStream();
-                    networkStream.ReadTimeout = 2000;
-
-                    // recv from SocketIO, send to TCP socket
-                    client.On("echo", response =>
-                    {
-                        foreach (var b in response.ToString())
-                        {
-                            networkStream.WriteByte((byte)b);
-                        }
-                        networkStream.Flush();
-                    });
-
-
-                    // recv from TCP socket, send to SocketIO
-                    bool connected = true; // This value should be able to be changed based on EITHER end of the pipe
-                    while (connected)
-                    {
-                        byte[] buffer = new byte[1024];
-                        int count = 0;
-                        if (networkStream.DataAvailable && count < 1024)
-                        {
-                            buffer[count] = (byte)networkStream.ReadByte();
-                            count++;
-                        }
-                        if(count != 0)
-                        {
-                            await client.EmitAsync("echo", Convert.ToBase64String(buffer));
-                        }
-                        
-                    }
-                }
-        
-            }
-            */
-
-            // finally, connect to the server and start the party
             await client.ConnectAsync();
+            new TcpPortForwarder(client, targetPort, targetHost).Start();
+        }
+    }
+    internal sealed class TcpPortForwarder
+    {
+        private readonly SocketIO client;
+        private readonly string _targetHost;
+        private readonly int _targetPort;
+
+        public TcpPortForwarder(SocketIO sio, int targetPort, string targetHost)
+        {
+            client = sio;
+            _targetPort = targetPort;
+            _targetHost = targetHost;
         }
 
+        public void Start()
+        {
+            client.On("echo", response =>
+            {
+                var clientPair = new ClientPair();
+                try
+                {
+                    clientPair.message = response.GetValue(0).GetProperty("data").ToString();
+                    clientPair.id = response.GetValue(0).GetProperty("id").GetInt32();
+                    clientPair.connectRetryCount = 0;
+                    clientPair.disconnected = false;
+                    clientPair.target = new TcpClient();
+                    clientPair.target.BeginConnect(_targetHost, _targetPort, TargetConnect, clientPair);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Failed when trying to accept new clients with '{0}'", (object)ex.ToString());
+                }
+            });
+        }
+
+        private void TargetConnect(IAsyncResult asyncResult)
+        {
+            var clientPair = asyncResult.AsyncState != null
+                ? (ClientPair)asyncResult.AsyncState
+                : throw new ArgumentNullException(nameof(asyncResult));
+            try
+            {
+                clientPair.target.EndConnect(asyncResult);
+                clientPair.targetStream = clientPair.target.GetStream();
+
+                SourceRead(clientPair);
+                clientPair.targetStream.BeginRead(clientPair.targetBuffer, 0, clientPair.targetBuffer.Length,
+                    TargetRead, clientPair);
+            }
+            catch (SocketException ex)
+            {
+                if (clientPair.connectRetryCount < 2)
+                {
+                    ++clientPair.connectRetryCount;
+                    clientPair.target.BeginConnect(_targetHost, _targetPort, TargetConnect, clientPair);
+                    Console.WriteLine("Retrying connect");
+                }
+                else
+                {
+                    Console.WriteLine("Connection failed: {0}", (object)ex.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed connecting to target with '{0}'", (object)ex.ToString());
+            }
+        }
+
+        private void SourceRead(ClientPair asyncState)
+        {
+            try
+            {
+                var data = Convert.FromBase64String(asyncState.message);
+                var count = data.Length;
+                if (count > 0)
+                {
+                    if (asyncState.target.Connected)
+                    {
+                        asyncState.targetStream.BeginWrite(data, 0, count, TargetWrite, asyncState);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Client disconnected: '{0}'", (object)ex.Message);
+            }
+        }
+
+        private void TargetRead(IAsyncResult asyncResult)
+        {
+            var asyncState = asyncResult.AsyncState as ClientPair;
+            if (!asyncState.disconnected)
+                if (asyncState.target.Connected)
+                    try
+                    {
+                        var count = asyncState.targetStream.EndRead(asyncResult);
+                        if (count > 0) {
+                            client.EmitAsync("echo", new
+                            {
+                                id = asyncState.id,
+                                data = Convert.ToBase64String(asyncState.targetBuffer.Take(count).ToArray())
+                            });
+
+                            try
+                            {
+                                asyncState.targetStream.BeginRead(asyncState.targetBuffer, 0, asyncState.targetBuffer.Length, TargetRead, asyncState);
+                            }
+                            catch
+                            {
+                                DisconnectPair(asyncState);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Server disconnected '{0}'", (object)ex.Message);
+                    }
+
+            if (asyncState.disconnected)
+                return;
+            DisconnectPair(asyncState);
+        }
+
+        private void DisconnectPair(ClientPair pair)
+        {
+            if (pair.disconnected)
+                return;
+            try
+            {
+                try
+                {
+                    if (pair.target.Client.Connected)
+                        pair.target.Client.Close();
+                }
+                catch {}
+
+                if (!pair.disconnected)
+                {
+                    pair.disconnected = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private void TargetWrite(IAsyncResult asyncResult)
+        {
+            var pair = asyncResult.AsyncState != null
+                ? asyncResult.AsyncState as ClientPair
+                : throw new ArgumentNullException(nameof(asyncResult));
+
+            try
+            {
+                pair.targetStream.EndWrite(asyncResult);
+            }
+            catch
+            {
+                if (!pair.disconnected)
+                    DisconnectPair(pair);
+            }
+        }
+    }
+
+    internal class ClientPair
+    {
+        public int id;
+        public string message;
+        public readonly byte[] sourceBuffer = new byte[65536];
+        public readonly byte[] targetBuffer = new byte[65536];
+        public int connectRetryCount;
+        public bool disconnected;
+        public TcpClient target;
+        public NetworkStream targetStream;
     }
 }
-
--- proxy_work/socket_server.js
+++ proxy_work/socket_server.js
@@ -1,71 +1,63 @@
 const express = require('express');
 const app = express();
+const net = require("net");
 const listener = require('http').Server(app, {
   maxHttpBufferSize: 1e8
 });
-
-
-// TCP socket server
-const net = require("net");
 const port = 5000;
 
 const clients = require('socket.io')(listener);
-listener.listen(8888, () => {
-  console.log(`Listener server running at http://localhost:${8888}/`);
+listener.listen(8000, () => {
+  console.log(`Listener server running at http://localhost:${8000}/`);
 });
 
-
-// pasted from index.js
+var connectionCounter = 0;
+var connections = [];
 
 customers = [];
 
 clients.on('connection', (socket) => {
   console.log("new connection: " + socket.id);
-  clients.to(socket.id).emit("echo", "welcome");
 
-  // TCP socket code 
-  const server = net.createServer((tcp_sock) => {
-      console.log("Client connected");
-  
-      // TCP socket recv's some data ...
-      tcp_sock.on("data", (data) => {
-          const strData = data.toString();
-          console.log(`TCP Server Received: ${strData}`);
-          clients.to(socket.id).emit("echo", data.toString('base64'));
-      });
-
-      // websocket recv's some data
-      socket.on('echo', msg => {
-        data = Buffer.from(msg, 'base64');
-        tcp_sock.write(data);
-      });
-  
-      tcp_sock.on("end", () => {
-          console.log("Client disconnected");
-      });
-  
-      tcp_sock.on("error", (error) => {
-          console.log(`Socket Error: ${error.message}`);
-      });
-  });
-
-
-
-  server.on("error", (error) => {
-      console.log(`Server Error: ${error.message}`);
+  socket.on('echo', msg => {
+    data = Buffer.from(msg.data, 'base64');
+    console.log(msg.id + "from: " + msg.data);
+    connections[msg.id].write(data);
   });
   
-  server.listen(port, () => {
-      console.log(`TCP socket server is running on port: ${port}`);
-  }); // end TCP socket code
-
-  socket.on('disconnect', function() {
+  socket.on('disconnect', () => {
     console.log(socket.id + " disconnected");
-    // disconnect from socket
   });
-
-
 });
 
+// TCP socket code 
+const server = net.createServer(tcp_sock => {
+  console.log("Client connected");
 
+  var id = connectionCounter++;
+  connections[id] = tcp_sock;
 
+  // TCP socket recv's some data ...
+  tcp_sock.on("data", (data) => {
+      const strData = data.toString();
+      console.log(`TCP Server Received: ${strData}`);
+      clients.emit("echo", { id, data: data.toString('base64') });
+  });
+
+  tcp_sock.on("end", () => {
+      console.log("Client disconnected");
+      connections[id] = null;
+  });
+
+  tcp_sock.on("error", (error) => {
+      console.log(`Socket Error: ${error.message}`);
+  });
+});
+
+server.on("error", (error) => {
+  console.log(`Server Error: ${error.message}`);
+});
+
+server.listen(port, () => {
+  console.log(`TCP socket server is running on port: ${port}`);
+}); // end TCP socket code
